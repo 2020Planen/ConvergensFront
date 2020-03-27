98767815f704f4bd944e33b886a667ca1c66294d
RasmusLynge <RasmuslJacobsen@live.com>
2020-03-27 11:57:22 +0100

    navbar cleaned up and useState-warning in navbar fixed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
-import React, { Component, useState, useEffect } from "react";
+import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import {
-  Navbar,
-  Nav,
-  NavDropdown,
-  Jumbotron,
-  Container
-} from "react-bootstrap";
+import { Jumbotron, Container } from "react-bootstrap";
 import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./components/Home";
 import Send from "./components/Send";
@@ -15,9 +9,9 @@ import CreateRoutingSlip from "./components/routingSlip/CreateRoutingSlip";
 import EditRoutingSlip from "./components/routingSlip/EditRoutingSlip";
 import ErrorReciever from "./components/reciever/ErrorReciever";
 //import About from "./components/About";
+import Header from "./components/Navbar";
 import Test from "./components/test/Test";
 import Keycloak from "keycloak-js";
-import JWT from "jwt-decode";
 
 require("dotenv").config();
 
@@ -25,147 +19,9 @@ function NoMatch() {
   return (
     <Container>
       <Jumbotron>
-        <h3>404 - siden kan ikke findes</h3>
+        <h3>404 - Ups siden findes ikke ?</h3>
       </Jumbotron>
     </Container>
-  );
-}
-
-function Header({ keycloak }) {
-  const [userInfo, setInfo] = useState({
-    username: "",
-    email: "",
-    email_verified: false
-  });
-  useEffect(() => {
-    keycloak.loadUserInfo().then(userInfo => {
-      setInfo({
-        username: userInfo.preferred_username,
-        email: userInfo.email,
-        email_verified: userInfo.email_verified
-      });
-    });
-  }, []);
-
-  return (
-    <>
-      <Navbar
-        collapseOnSelect
-        expand="sm"
-        bg="dark"
-        variant="dark"
-        sticky="top"
-      >
-        <Navbar.Brand href="#home">Convergens</Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          {localStorage.getItem("keycloak-token") ? (
-            <>
-              <Nav className="mr-auto">
-                {JWT(
-                  localStorage.getItem("keycloak-token")
-                ).realm_access.roles.includes("superuser") ||
-                JWT(
-                  localStorage.getItem("keycloak-token")
-                ).realm_access.roles.includes("user") ? (
-                  <>
-                    <Nav.Link href="#send">Send Besked</Nav.Link>
-                    <NavDropdown
-                      title="Routing Slips"
-                      id="collasible-nav-dropdown"
-                    >
-                      <NavDropdown.Item href="#createSimpleRoutingSlip">
-                        Opret simpel routing slip
-                      </NavDropdown.Item>
-                      {JWT(
-                        localStorage.getItem("keycloak-token")
-                      ).realm_access.roles.includes("superuser") ? (
-                        <>
-                          <NavDropdown.Divider />
-                          <NavDropdown.Item href="#createRoutingSlip">
-                            Opret routing slip
-                          </NavDropdown.Item>
-                          <NavDropdown.Divider />
-                          <NavDropdown.Item href="#editRoutingSlip">
-                            Ændring af routing slips
-                          </NavDropdown.Item>
-                        </>
-                      ) : null}
-                    </NavDropdown>
-
-                    <NavDropdown
-                      title="Modtag beskeder"
-                      id="collasible-nav-dropdown"
-                    >
-                      <NavDropdown.Item href="#reciever">
-                        Modtag Beskeder
-                      </NavDropdown.Item>
-                      <NavDropdown.Divider />
-                      <NavDropdown.Item href="#errorReciever">
-                        Modtag Fejlbeskeder
-                      </NavDropdown.Item>
-                    </NavDropdown>
-                  </>
-                ) : null}
-              </Nav>
-              <Nav>
-                {JWT(
-                  localStorage.getItem("keycloak-token")
-                ).realm_access.roles.includes("admin") ? (
-                  <Nav.Link
-                    
-                  href="#test"
-                  >
-                    Test
-                  </Nav.Link>
-                ) : null}
-
-                <NavDropdown
-                  drop="left"
-                  title={userInfo.username}
-                  id="user-nav-dropdown"
-                >
-                  {userInfo.email !== undefined ? (
-                    <>
-                      <NavDropdown.Item>{userInfo.email}</NavDropdown.Item>
-                      <NavDropdown.Divider />
-                    </>
-                  ) : null}
-                  <NavDropdown.Item
-                    href={
-                      JWT(localStorage.getItem("keycloak-token")).iss +
-                      "/account/"
-                    }
-                  >
-                    Rediger bruger
-                  </NavDropdown.Item>
-                  <NavDropdown.Divider />
-                  <NavDropdown.Item
-                    onClick={() => {
-                      keycloak.logout();
-                      localStorage.clear();
-                    }}
-                  >
-                    log ud
-                  </NavDropdown.Item>
-                </NavDropdown>
-              </Nav>
-            </>
-          ) : (
-            <Nav>
-              <Nav.Link
-                onClick={() => {
-                  keycloak.logout();
-                  localStorage.clear();
-                }}
-              >
-                log ind igen
-              </Nav.Link>
-            </Nav>
-          )}
-        </Navbar.Collapse>
-      </Navbar>
-    </>
   );
 }
 
@@ -222,18 +78,14 @@ class App extends Component {
                   path="/errorReciever"
                   render={() => <ErrorReciever />}
                 />
-                {/*
-                <Route exact path="/test" render={() => <Test />} />
-                <Route exact path="/about" render={() => <About />} />
-                */}
                 <Route component={NoMatch} />
               </Switch>
             </Router>
           </>
         );
-      else return <div>Unable to authenticate!</div>;
+      else return <div>Kunne ikke godkende.</div>;
     }
-    return <div>Verificere login...</div>;
+    return <div>Vent venligst...</div>;
   }
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,138 @@
+import React, { useState, useEffect } from "react";
+import { Navbar, Nav, NavDropdown } from "react-bootstrap";
+import JWT from "jwt-decode";
+
+function Header({ keycloak }) {
+  const [userInfo, setInfo] = useState({
+    username: "",
+    email: "",
+    email_verified: false
+  });
+  useEffect(() => {
+    keycloak.loadUserInfo().then(userInfo => {
+      setInfo({
+        username: userInfo.preferred_username,
+        email: userInfo.email,
+        email_verified: userInfo.email_verified
+      });
+    });
+  });
+
+  return (
+    <>
+      <Navbar
+        collapseOnSelect
+        expand="sm"
+        bg="dark"
+        variant="dark"
+        sticky="top"
+      >
+        <Navbar.Brand href="#home">Convergens</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          {localStorage.getItem("keycloak-token") ? (
+            <>
+              <Nav className="mr-auto">
+                {JWT(
+                  localStorage.getItem("keycloak-token")
+                ).realm_access.roles.includes("superuser") ||
+                JWT(
+                  localStorage.getItem("keycloak-token")
+                ).realm_access.roles.includes("user") ? (
+                  <>
+                    <Nav.Link href="#send">Send Besked</Nav.Link>
+                    <NavDropdown
+                      title="Routing Slips"
+                      id="collasible-nav-dropdown"
+                    >
+                      <NavDropdown.Item href="#createSimpleRoutingSlip">
+                        Opret simpel routing slip
+                      </NavDropdown.Item>
+                      {JWT(
+                        localStorage.getItem("keycloak-token")
+                      ).realm_access.roles.includes("superuser") ? (
+                        <>
+                          <NavDropdown.Divider />
+                          <NavDropdown.Item href="#createRoutingSlip">
+                            Opret routing slip
+                          </NavDropdown.Item>
+                          <NavDropdown.Divider />
+                          <NavDropdown.Item href="#editRoutingSlip">
+                            Ændring af routing slips
+                          </NavDropdown.Item>
+                        </>
+                      ) : null}
+                    </NavDropdown>
+
+                    <NavDropdown
+                      title="Modtag beskeder"
+                      id="collasible-nav-dropdown"
+                    >
+                      <NavDropdown.Item href="#reciever">
+                        Modtag Beskeder
+                      </NavDropdown.Item>
+                      <NavDropdown.Divider />
+                      <NavDropdown.Item href="#errorReciever">
+                        Modtag Fejlbeskeder
+                      </NavDropdown.Item>
+                    </NavDropdown>
+                  </>
+                ) : null}
+              </Nav>
+              <Nav>
+                {JWT(
+                  localStorage.getItem("keycloak-token")
+                ).realm_access.roles.includes("admin") ? (
+                  <Nav.Link href="#test">Test</Nav.Link>
+                ) : null}
+
+                <NavDropdown
+                  drop="left"
+                  title={userInfo.username}
+                  id="user-nav-dropdown"
+                >
+                  {userInfo.email !== undefined ? (
+                    <>
+                      <NavDropdown.Item>{userInfo.email}</NavDropdown.Item>
+                      <NavDropdown.Divider />
+                    </>
+                  ) : null}
+                  <NavDropdown.Item
+                    href={
+                      JWT(localStorage.getItem("keycloak-token")).iss +
+                      "/account/"
+                    }
+                  >
+                    Rediger bruger
+                  </NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item
+                    onClick={() => {
+                      keycloak.logout();
+                      localStorage.clear();
+                    }}
+                  >
+                    log ud
+                  </NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
+            </>
+          ) : (
+            <Nav>
+              <Nav.Link
+                onClick={() => {
+                  keycloak.logout();
+                  localStorage.clear();
+                }}
+              >
+                log ind igen
+              </Nav.Link>
+            </Nav>
+          )}
+        </Navbar.Collapse>
+      </Navbar>
+    </>
+  );
+}
+
+export default Header;
